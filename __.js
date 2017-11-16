976eb3414f73ee815a853baa09cce532cdb23576
Juergen Riemer <juergen.riemer@gmail.com>
2017-11-16 06:56:27 +0100

    added utils

--- __.js
+++ __.js
@@ -1,4 +1,8 @@
-
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @output_file_name __.min.js
+// @js_externs var __; __.dn_; __.dn._dn; __.dn.del; __.dn; __.dn._move; __.dn.move; __.dn.move_; __.dn._add; __.dn.add; __.dn.add_; __.dn.hide; __.dn.show; __.dn.ix; __.dn.x; __.dn.y; __.dn.dx; __.dn.dy; __.each; __.css; __.e; __.b; __.b.email; __.b.empty; __.b.url; __.cookies; __.cookies.get; __.cookies.set; __.cookies.del; __.url; __.url.oParams; __.o; __.o.s; __.o.add; __.o.kRename; __.o.clone; __.o.k; __.s; __.s.o;
+// ==/ClosureCompiler==
 // version 1.5
 __ = {
 	dn_ : function( s, x1, x2 ) {
@@ -64,13 +68,13 @@ __ = {
 			dnExisting.parentNode.insertBefore( dnNew, dnExisting.nextSibling );
 		}
 		, _add : function( h, x1, x2 ) {
-			__.dn._add_( h, x1, x2, "_move" );
+			return __.dn._add_( h, x1, x2, "_move" );
 		}
 		, add : function( h, x1, x2 ) {
-			__.dn._add_( h, x1, x2, "move" );
+			return __.dn._add_( h, x1, x2, "move" );
 		}
 		, add_ : function( h, x1, x2 ) {
-			__.dn._add_( h, x1, x2, "move_" );
+			return __.dn._add_( h, x1, x2, "move_" );
 		}
 		, _add_ : function( h, x1, x2, sfn ) {
 			var dnRoot = ( typeof x1 == "object" ) ? x1 : document.body;
@@ -111,11 +115,148 @@ __ = {
 			dn.setAttribute( "__.display", sDisplay ); 
 			dn.style.display = "none";
 		}
+		, ix : function( dn ) {
+			var ix = 0;
+			while( ( dn = dn.previousSibling ) != null ) {
+				ix++;
+			}
+			return ix;
+		}
+		, x : function( dn, x ) {
+			if( x ) {
+				dn.style.left = parseInt( x ) + "px";
+			}
+			else {
+				return dn.getBoundingClientRect().left;
+			}
+		}
+		, y : function( dn, y ) {
+			if( y ) {
+				dn.style.top = parseInt( y ) + "px";
+			}
+			else {
+				return dn.getBoundingClientRect().top;
+			}
+		}
+		, dx : function( dn, dx ) {
+			if( dx ) {
+				dn.style.width = parseInt( dx ) + "px";
+			}
+			else {
+				return dn.getBoundingClientRect().width;
+			}
+		}
+		, dy : function( dn, dy ) {
+			if( dy ) {
+				dn.style.height = parseInt( dy ) + "px";
+			}
+			else {
+				return dn.getBoundingClientRect().height;
+			}
+		}
 	}
+	, s : {
+		o : function( s ) {
+			try {
+				return JSON.parse( s );
+			}
+			catch( e ) {
+				return null;
+			}
+		}
+	}
+	, o : {
+		  s : function( o ) {
+			return JSON.stringify( o );
+		}
+		, add : function( ocur, onew ) {
+			for( var s in onew ) {
+				ocur[ s ] = onew[ s ];
+			} 
+		}
+		// http://stackoverflow.com/a/14592469/463676
+		, kRename : function( o, kold, knew ) {
+			Object.defineProperty(
+				  o
+				, knew
+				, Object.getOwnPropertyDescriptor( o, kold )
+			);
+			delete o[ kold ];
+		}
+		, clone : function( o ) {
+			try {
+				return JSON.parse( JSON.stringify( o ) );
+			}
+			catch( e ) {
+				return null;
+			}
+		}
+		, k : function( o ) {
+			for( k in o ) {
+				return k;
+			};
+		}
+	};
+
 	, css : function( sStyle ) {
 		var dn = document.createElement( 'style' );
 		document.body.appendChild( dn );
 		dn.innerHTML = sStyle;
+	}
+	, e : {
+		stop : function( e ) {
+			e.stopPropagation();
+			e.preventDefault();
+		}
+	}
+	, b : {
+		  email : function( s ) {
+			return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+[\.]{1}[a-zA-Z]{2,4}$/.test( s );
+		}
+		, url : function( s ) {
+			return /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test( v );
+		}
+		, empty : function( x ) {
+			if( typeof x == "string" ) {
+				return ( x == "" );
+			}
+			else if( x.length ) {
+				return ( x.length == 0 );
+			}
+			else if( typeof x == "object" ) {
+				// http://stackoverflow.com/a/34491287/463676
+				for( var dy in o ) {
+					return false;
+				}
+				return true;
+			}
+		}
+	}
+	, cookies : {
+		  get : function( k ) {
+			var v = document.cookie.match( '(^|;) ?' + k + '=([^;]*)(;|$)' );
+			return v ? v[ 2 ] : null;
+		}
+		, set : function( k, v, nDays ) {
+			var d = new Date;
+			d.setTime( d.getTime() + 86400000 * nDays );
+			document.cookie = k + "=" + v + ";path=/;expires=" + d.toGMTString();
+		}
+		, del : function( k ) {
+			__.cookie.set( k, '', -1 );
+		}
+	}
+	, url : {
+		  oParams : function () {
+			var o = {};
+			window.location.href.replace(
+				/[?&]+([^=&]+)=([^&]*)/gi,
+				function( m, k, v ) {
+					o[ k ] = v;
+				}
+			);
+			return o;
+		}
 	}
 }
 
@@ -127,11 +268,11 @@ this.Element && function( oPrototype ) {
 	oPrototype.webkitMatchesSelector ||
 	oPrototype.msMatchesSelector ||
 	function( sCSS ) {
-		var dn = this
+		var dn = this;
 		var ldn = ( dn.parentNode || dn.document ).querySelectorAll( sCSS )
-		var i = -1;
-		while( ldn[ ++i ] && ldn[ i ] != dn );
-		return !! ldn[ i ];
+		var ix = -1;
+		while( ldn[ ++ix ] && ldn[ ix ] != dn ){};
+		return !! ldn[ ix ];
 	}
 }( Element.prototype );
 
@@ -165,4 +306,8 @@ this.Element && function( oPrototype ) {
 		}
 	};
 }() );
+
+
+
+
 
