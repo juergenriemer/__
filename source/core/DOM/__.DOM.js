c7d43de947fd8ef0f8602f5478780167859a8c22
Juergen Riemer <juergen.riemer@gmail.com>
2018-04-30 09:52:26 +0200

    refactor

--- source/core/DOM/__.DOM.js
+++ source/core/DOM/__.DOM.js
@@ -13,109 +13,9 @@ if( typeof __ == "undefined" ) {
 	__ = {};
 }
 
-/**
- * Takes a CSS selector string and queries the DOM for matching nodes.
- * <br>
- * It returns a single DOM node if just one node was found.
- * <br>
- * It returns a list of DOM nodes if multiple were found.
- * <br>
- * It will return [null] in case nothing was found.
- * <br>
- * An optional DOM node as starting point can be passed on otherwise [document] is used.
- * <br>
- * An optional callback function to be applied on returned nodes can be passed on which
- * will be invoked with two parameters: the single node and its index of the array.
- * @memberof __
- * @method dn_
- * @example var dnMenu = __.dn_( "#menu" );
- * @example var ldnLinks = __.dn_( "a.footer", dnMenu );
- * @example __.dn_( "a.footer", dnMenu, function( dn, ix ) {
- *       dn.style.color = "red";
- *       dn.style.border = ix + "px solid green";
- *  } );
- * @example __.dn_( "[href]", function( dn ) {
- *       dn.setAttribute( "href", "#" );
- *  } );
- * @param {String} css CSS selector string
- * @param {Element|Function} [dn_or_cb] starting node or callback function
- * @param {Function} [cb] callback function
- * @returns {Element|Array} A DOM node or a list of DOM nodes or null if CSS selector could not be found
- */
-__.dn_ = function( css, dn_or_cb, cb ) {
-	var dnStart = ( typeof dn_or_cb == "object" ) ? dn_or_cb : document;
-	var cb = ( typeof dn_or_cb == "function" ) ? dn_or_cb : cb;
-	var sfn = "querySelectorAll";
-	if( /^.[a-zA-Z0-9_-]*$/.test( css ) ) {
-		var sfst = css.substr( 0, 1 );
-		if( sfst == "#" ) {
-			sfn = "getElementById";
-			css = css.substr( 1 );
-		}
-		else if( sfst == "." ) {
-			sfn = "getElementsByClassName";
-			css = css.substr( 1 );
-		}
-		else if( /[a-zA-Z]/.test( sfst ) ) {
-			sfn = "getElementsByTagName";
-		}
-	}
-	var xdn = dnStart[ sfn ]( css );
-	xdn = HTMLCollection.prototype.isPrototypeOf( xdn )
-		? [].slice.call( xdn ) : xdn;
-	if( xdn ) {
-		if( cb ) {
-			this.dn.each( xdn, cb );
-		}
-		var c = xdn.length;
-		if ( ! isNaN( c ) ) {
-			if( c == 1 ) {
-				return xdn[ 0 ];
-			}
-			else if( c == 0 ) {
-				return null;
-			}
-		}
-	}
-	return xdn;
-};
-
-/**
- * Takes a CSS selector string and queries the DOM for the closest matching parent node.
- * <br>
- * It returns a DOM node. It will return [null] in case nothing was found.
- * <br>
- * An optional callback function to be applied on the returned node. It
- * will be invoked with one parameter: the single node
- * @memberof __
- * @method _dn
- * @example var dnInput = __.dn_( "[name='email']" );
- * var dnForm = __._dn( "form", dnInput );
- * @example __._dn( "div.the-form", dnInput, function( dn ) {
- *     dn.style.border = "1px solid red";
- *     dn.style.padding = "1em";
- * } );
- * @param {String} css CSS selector string
- * @param {Element} dn starting node
- * @param {Function} [cb] callback function
- * @returns {Element} A DOM node or null if CSS selector could not be found
- */
-__._dn = function( css, dn, cb ) {
-	var dnClosest = dn.closest( css );
-	if( cb ) {
-		this.dn.each( dnClosest, cb );
-	}
-	return dnClosest
-};
-
-
-/**
- * Provides methods that operate on DOM nodes.
- * @memberof __
- * @type {object}
- * @namespace __.dn
- */
-__.dn = {
+if( typeof __.utils == "undefined" ) {
+	__.utils = {};
+}
 	/**
 	 * Receives either an element or an array and a callback function.
 	 * <br>
@@ -142,109 +42,240 @@ __.dn = {
 	 * @param {Array|Element} xdn Array of nodes or a single node 
 	 * @param {Function} fn function to be invoked against nodes
 	 */
-	  each : function( xdn, fn ) {
-		if( xdn ) {
-			var ldn = ( ! isNaN( xdn.length ) ) ? xdn : [ xdn ];
-			var c = ldn.length;
-			for( var ix=0; ix<c; ix++ ) {
-				fn( ldn[ ix ], ix );
+__.utils.each = function( xdn, fn ) {
+	if( xdn ) {
+		var ldn = ( ! isNaN( xdn.length ) ) ? xdn : [ xdn ];
+		var c = ldn.length;
+		for( var ix=0; ix<c; ix++ ) {
+			fn( ldn[ ix ], ix );
+		}
+	}
+}
+
+__.dn = {
+
+};
+
+/**
+ * Takes a CSS selector string and queries the DOM for matching nodes.
+ * <br>
+ * It returns a single DOM node if just one node was found.
+ * <br>
+ * It returns a list of DOM nodes if multiple were found.
+ * <br>
+ * It will return [null] in case nothing was found.
+ * <br>
+ * An optional DOM node as starting point can be passed on otherwise [document] is used.
+ * <br>
+ * An optional callback function to be applied on returned nodes can be passed on which
+ * will be invoked with two parameters: the single node and its index of the array.
+ * @memberof Node
+ * @method find
+ * @example var dnMenu = document.body.find( "#menu" );
+ * @example var ldnLinks = dnMenu.find( "a.footer" );
+ * @example dnMenu.find( "a.footer", function( dn, ix ) {
+ *       dn.style.color = "red";
+ *       dn.style.border = ix + "px solid green";
+ *  } );
+ * @example document.body.find( "[href]", function( dn ) {
+ *       dn.setAttribute( "href", "#" );
+ *  } );
+ * @param {String} css CSS selector string
+ * @param {Function} [cb] callback function
+ * @returns {Element|Array} A DOM node or a list of DOM nodes or null if CSS selector could not be found
+ */
+
+Object.defineProperty( Node.prototype, "find", {
+	value : function( css, cb ) {
+		var dn = ( this.tagName.toUpperCase() == "BODY" )
+			? document
+			: this;
+		var sfn = "querySelectorAll";
+		if( /^.[a-zA-Z0-9_-]*$/.test( css ) ) {
+			var sfst = css.substr( 0, 1 );
+			if( sfst == "#" ) {
+				sfn = "getElementById";
+				css = css.substr( 1 );
+			}
+			else if( sfst == "." ) {
+				sfn = "getElementsByClassName";
+				css = css.substr( 1 );
+			}
+			else if( /[a-zA-Z]/.test( sfst ) ) {
+				sfn = "getElementsByTagName";
 			}
 		}
+		var xdn = dn[ sfn ]( css );
+		xdn = HTMLCollection.prototype.isPrototypeOf( xdn )
+			? [].slice.call( xdn ) : xdn;
+		if( xdn ) {
+			if( cb ) {
+				__.utils.each( xdn, cb );
+			}
+			var c = xdn.length;
+			if ( ! isNaN( c ) ) {
+				if( c == 1 ) {
+					return xdn[ 0 ];
+				}
+				else if( c == 0 ) {
+					return null;
+				}
+			}
+		}
+		return xdn;
 	}
-	/**
-	* Deletes a DOM node.
-	* @memberof __.dn
-	* @method del
-	* @param {Element} dn DOM node to be deleted
-	* @example var dnForm = __.dn_( "form" );
-	* __.dn.del( dnForm );
-	*/
-	, del : function( dn ) {
-		dn.parentNode.removeChild( dn );
+} );
+/**
+* Deletes a DOM node.
+* @memberof Node
+* @method remove
+* @example var dnForm = document.body.find( "form" );
+* dnForm.remove();
+*/
+Object.defineProperty( Node.prototype, "remove", {
+	value : function() {
+		this.parentNode.removeChild( this );
 	}
-	/**
-	* <pre>
-	* Takes an HTML string, converts it into DOM node(s) and writes inside of an existing DOM node.
-	* It returns a single DOM node if HTML string has one root tag.
-	* It returns a list of DOM nodes if HTML string has multiple root tags.
-	* An optional DOM node as target DOM node can be passed on otherwise [document] is used.
-	* An optional callback function to be applied on created nodes can be passed on which will be
-	* invoked with two parameters: the single node and its index of the array
-	* </pre>
-	* @memberof __.dn
-	* @method append
-	* @example var dnUL = __.dn.append( "<ul class='colors'></ul>" );
-	* @example var h = "<li>red</li>";
-	* var lnLI = __.dn.append( h, dnUL );
-	* @example var h = "<li>red</li>";
-	* var lnLI = __.dn.append( h, dnUL, function( dn, ix ) {
-	*     dn.style.color = dn.textContent;
-	*     dn.textContent = ix + ". " + dn.textContent;
-	* } );
-	* @param {String} h String of valid HTML.
-	* @param {Element|Function} [x1] target node or callback function
-	* @param {Function} [x2] callback function
-	* @returns {Element|Array} A DOM node or a list of DOM nodes
-	*/
-	, append : function( h, x1, x2 ) {
-		return __.dn._add_( h, x1, x2, "move" );
+} );
+
+/**
+ * Takes a CSS selector string and queries the DOM for the closest matching parent node.
+ * <br>
+ * It returns a DOM node. It will return [null] in case nothing was found.
+ * <br>
+ * An optional callback function to be applied on the returned node. It
+ * will be invoked with one parameter: the single node
+ * @memberof __
+ * @method _dn
+ * @example var dnInput = __.dn_( "[name='email']" );
+ * var dnForm = __._dn( "form", dnInput );
+ * @example __._dn( "div.the-form", dnInput, function( dn ) {
+ *     dn.style.border = "1px solid red";
+ *     dn.style.padding = "1em";
+ * } );
+ * @param {String} css CSS selector string
+ * @param {Element} dn starting node
+ * @param {Function} [cb] callback function
+ * @returns {Element} A DOM node or null if CSS selector could not be found
+ */
+Object.defineProperty( Node.prototype, "closest", {
+	value : function( css, cb ) {
+console.log( ">>>>>>>>>" );
+		var dnClosest = this.closest( css );
+		if( cb ) {
+			__.utils.each( dnClosest, cb );
+		}
+		return dnClosest
 	}
-	/**
-	* <pre>
-	* xxxxxxTakes an HTML string, converts it into DOM node(s) and writes inside of an existing DOM node.
-	* It returns a single DOM node if HTML string has one root tag.
-	* It returns a list of DOM nodes if HTML string has multiple root tags.
-	* An optional DOM node as target DOM node can be passed on otherwise [document] is used.
-	* An optional callback function to be applied on created nodes can be passed on which will be
-	* invoked with two parameters: the single node and its index of the array
-	* </pre>
-	* @memberof __.dn
-	* @method prepend
-	* @param {String} h String of valid HTML.
-	* @param {Element|Function} [x1] target node or callback function
-	* @param {Function} [x2] callback function
-	* @returns {Element|Array} A DOM node or a list of DOM nodes
-	*/
-	, prepend : function( dnMove, dnTarget ) {
-		if( dnTarget.firstChild ) {
-			this.before( dnMove, dnTarget.firstChild );
+} );
+
+/**
+* Takes either an HTML string and converts it into DOM node(s) or (an) 
+* existing exsting DOM node(s) and creates or moves them inside the
+* DOM node at the top. 
+* It returns the created or moved DOM node(s).
+* An optional callback function to be applied on created or moved DOM
+* node(s) can be passed on which will be
+* invoked with two parameters: the single node and its index of the array.
+* @memberof Node
+* @method __append
+* @example var dnUL = document.body.__append( "<ul class='colors'></ul>" );
+* @param {String} h_or_dn String of valid HTML.
+* @param {Function} [cb] target node or callback function
+* @returns {Element|Array} A DOM node or a list of DOM nodes
+*/
+
+Object.defineProperty( Node.prototype, "__append", {
+	value : function( h_or_dn, cb ) {
+		return __.dn._add_( h_or_dn, this, cb, "move" );
+	}
+} );
+
+/**
+* Takes either an HTML string and converts it into DOM node(s) or (an) 
+* existing exsting DOM node(s) and creates or moves them inside the
+* DOM node at bottom.
+* It returns the created or moved DOM node(s).
+* An optional callback function to be applied on created or moved DOM
+* node(s) can be passed on which will be
+* invoked with two parameters: the single node and its index of the array.
+* @memberof Node
+* @method __prepend 
+* @example var dnUL = document.body.__prepend( "<ul class='colors'></ul>" );
+* @example var h = "<li>red</li>";
+* var lnLI = __.dn.__prepend( h, dnUL );
+* @example var h = "<li>red</li>";
+* var lnLI = __.dn.append( h, dnUL, function( dn, ix ) {
+*     dn.style.color = dn.textContent;
+*     dn.textContent = ix + ". " + dn.textContent;
+* } );
+* @param {String} h_or_dn String of valid HTML.
+* @param {Function} [cb] target node or callback function
+* @returns {Element|Array} A DOM node or a list of DOM nodes
+*/
+
+Object.defineProperty( Node.prototype, "__prepend", {
+	value : function( h_or_dn, cb ) {
+		if( this.firstChild ) {
+			return __.dn._add_( h_or_dn, this, cb, "_move" );
 		}
 		else {
-			this.append( dnMove, dnTarget );
+			return __.dn._add_( h_or_dn, this, cb, "move" );
 		}
 	  }
-	/**
-	* Same as [add] with the difference that the DOM node(s) are not inserted into the existing DOM
-	* node but right in front of it.
-	* @memberof __.dn
-	* @method before
-	* @example var dnLogin = __.dn_( "[name='login']" );
-	* var h = "<div>At least 8 characters</div>";
-	* var dnRule = __.dn.before( h, dnLogin );
-	* @param {String} h String of valid HTML.
-	* @param {Element|Function} [x1] target node or callback function
-	* @param {Function} [x2] callback function
-	* @returns {Element|Array} A DOM node or a list of DOM nodes
-	*/
-	, before : function( h, x1, x2 ) {
-		return __.dn._add_( h, x1, x2, "_move" );
+} );
+
+/**
+* Takes either an HTML string and converts it into DOM node(s) or (an) 
+* existing existing DOM node(s) and creates or moves them right before the
+* DOM node.
+* It returns the created or moved DOM node(s).
+* An optional callback function to be applied on created or moved DOM
+* node(s) can be passed on which will be
+* invoked with two parameters: the single node and its index of the array.
+* @memberof Node
+* @method __before
+* @param {String} h_or_dn String of valid HTML.
+* @param {Function} [cb] target node or callback function
+* @returns {Element|Array} A DOM node or a list of DOM nodes
+*/
+
+Object.defineProperty( Node.prototype, "__before", {
+	value : function( h_or_dn, cb ) {
+		return __.dn._add_( h_or_dn, cb, "_move" );
 	}
-	/**
-	* xxxxSame as [append] with the difference that the DOM node(s) are not inserted into the existing DOM
-	* node but right after of it.
-	* @memberof __.dn
-	* @method after
-	* @example var dnComment = __.dn_( "[name='comment']" );
-	* var h = "<a href='#clear'>clear</a>";
-	* var dnRule = __.dn.after( h, dnComment );
-	* @param {String} h String of valid HTML.
-	* @param {Element|Function} [x1] target node or callback function
-	* @param {Function} [x2] callback function
-	* @returns {Element|Array} A DOM node or a list of DOM nodes
-	*/
-	, after : function( h, x1, x2 ) {
-		return __.dn._add_( h, x1, x2, "move_" );
+} );
+
+
+/**
+* Takes either an HTML string and converts it into DOM node(s) or (an) 
+* existing existing DOM node(s) and creates or moves them right after the
+* DOM node.
+* It returns the created or moved DOM node(s).
+* An optional callback function to be applied on created or moved DOM
+* node(s) can be passed on which will be
+* invoked with two parameters: the single node and its index of the array.
+* @memberof Node
+* @method __after
+* @param {String} h_or_dn String of valid HTML.
+* @param {Function} [cb] target node or callback function
+* @returns {Element|Array} A DOM node or a list of DOM nodes
+*/
+
+Object.defineProperty( Node.prototype, "__after", {
+	value : function( h_or_dn, cb ) {
+		return __.dn._add_( h_or_dn, cb, "move_" );
 	}
+} );
+
+/**
+ * Provides methods that operate on DOM nodes.
+ * @memberof __
+ * @type {object}
+ * @namespace __.dn
+ */
+__.dn = {
+a:{}
 	/**
 	* <pre>
 	* Shows a DOM node.
@@ -463,6 +494,41 @@ __.dn = {
 		}
 		dn.style[ k ] = v;
 	}
+	/**
+	 * Receives either an element or an array and a callback function.
+	 * <br>
+	 * In case of an array it iterates and invokes the callback on each element.
+	 * In case an element it invokes the callback directly.
+	 * <br>
+	 * The callback is invoked with two parameters: the element and its index.
+	 * @memberof __.dn
+	 * @method each
+	 * @example var ldn = __.dn_( ".links" );
+	 * __.dn.each( ldn, function( dn, ix ) {
+	 *     console.log( ix + ". " + dn.tagName ); 
+	 * } );
+	 * @example var dn1 = __.dn_( "[href='#one']" );
+	 * var dn2 = __.dn_( "[href='#two']" );
+	 * var dn3 = __.dn_( "[href='#three']" );
+	 * var ldn = [ dn1, dn2, dn3 ];
+	 * __.dn.each( ldn, function( dn ) {
+	 *     dn.style.color = "red";
+	 * } );
+	 * @example __.dn.each( __.dn_( "a" ), function( dn ) {
+	 *     dn.style.color = "red";
+	 * } );
+	 * @param {Array|Element} xdn Array of nodes or a single node 
+	 * @param {Function} fn function to be invoked against nodes
+	 */
+	, each : function( xdn, fn ) {
+		if( xdn ) {
+			var ldn = ( ! isNaN( xdn.length ) ) ? xdn : [ xdn ];
+			var c = ldn.length;
+			for( var ix=0; ix<c; ix++ ) {
+				fn( ldn[ ix ], ix );
+			}
+		}
+	}
 	, h : function( dn, us_h ) {
 		us_h = us_h.toString() || "";
 		var dnWrapper = document.createElement( "div" );
@@ -501,7 +567,6 @@ __.dn = {
 			dnTarget.parentNode.insertBefore( dnMove, dnTarget.nextSibling );
 		}
 	}
-	// rename h to x for it can be a dn or h
 	, _add_ : function( h_or_dn, x1, x2, sfn ) {
 		var dnRoot = ( typeof x1 == "object" ) ? x1 : document.body;
 		var fn = ( typeof x1 == "function" ) ? x1 : x2;
